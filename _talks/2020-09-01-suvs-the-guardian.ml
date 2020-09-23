f7cbd5ec437ff3e35b1d8e4cce939d3084e0b027
sebcast <sebastianc@gmail.com>
2020-09-23 11:56:03 -0400

    Create 2020-09-01-suvs-the-guardian.ml

---
+++ _talks/2020-09-01-suvs-the-guardian.ml
@@ -0,0 +1,14 @@
+---
+title: "Quote from The Guardian: How SUVs conquered the world – at the expense of its climate"
+collection: talks
+type: "Newspaper quote"
+permalink: /talks/2020-09-01-suvs-the-guardian
+venue: "IFEMA"
+date: 2020-09-01
+location: "London"
+---
+Quote and inputs ona piece on the climate impacts of SUVs for The Guardian:
+https://www.theguardian.com/us-news/2020/sep/01/suv-conquered-america-climate-change-emissions
+“To avert the worst of the climate catastrophe, the transport sector needs to be completely decarbonized,” said Sebastian Castellanos, a researcher at the New Urban Mobility Alliance who calculated the emissions. “With the explosion in SUV sales, we are moving even farther away from our goal of decarbonizing the sector.”
+
+
